676ed8331965c85d5ff15f5e484643d5c9f1c30a
Gurwi <gs3335769@gmail.com>
2024-11-12 21:31:49 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,44 @@
 #![allow(unused_imports)]
 
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
 
 const DEFAULT_PORT: u16 = 6379;
 
-fn main() {
-    let listener = TcpListener::bind(("127.0.0.1", DEFAULT_PORT)).unwrap();
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("Connection established! Responding to ping...");
-                handle_client(&mut stream);
-            }
+    // for stream in listener.accept().await {
+    //     match stream {
+    //         Ok(stream) => {
+    //             println!("Connection established! Responding to ping...");
+    //             handle_client(stream);
+    //         }
+    //
+    //         Err(e) => {
+    //             println!("An error occurred: {:?}", e);
+    //         }
+    //     }
+    // }
 
-            Err(e) => {
-                println!("An error occurred: {:?}", e);
-            }
+    match listener.accept().await {
+        Ok((_socket, addr)) => {
+            println!("Connection established! {addr}...");
+            handle_client(_socket);
+        }
+
+        Err(e) => {
+            println!("An error occurred: {:?}", e);
         }
     }
 
+    Ok(())
+
 }
 
-fn handle_client(stream: &mut TcpStream) {
+fn handle_client(mut stream: TcpStream) {
     loop {
         let reads = stream.read(&mut [0; 256]).unwrap();
         if reads == 0 {
