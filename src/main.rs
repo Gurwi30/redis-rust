e8439c823c927e5134762d17eb995fad73992134
Gurwi <gs3335769@gmail.com>
2024-11-12 21:09:56 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 const DEFAULT_PORT: u16 = 6379;
@@ -8,18 +8,23 @@ const DEFAULT_PORT: u16 = 6379;
 fn main() {
     let listener = TcpListener::bind(("127.0.0.1", DEFAULT_PORT)).unwrap();
 
-    loop {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(mut stream) => {
-                    println!("Connection established! Responding to ping...");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("Connection established! Responding to ping...");
 
-                    stream.write_all(b"+PONG\r\n").unwrap()
+                loop {
+                    let reads = stream.read(&mut [0; 256]).unwrap();
+                    if reads <= 0 {
+                        break;
+                    }
                 }
 
-                Err(e) => {
-                    println!("An error occurred: {:?}", e);
-                }
+                stream.write_all(b"+PONG\r\n").unwrap()
+            }
+
+            Err(e) => {
+                println!("An error occurred: {:?}", e);
             }
         }
     }
