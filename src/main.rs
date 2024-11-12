25b5bd20bd634cbc9f2a3638e5ca2d69a2af73cd
Gurwi <gs3335769@gmail.com>
2024-11-12 21:45:45 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,6 @@ const DEFAULT_PORT: u16 = 6379;
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind(format!("127.0.0.1:{DEFAULT_PORT}")).await?;
 
-    // for stream in listener.accept().await {
-    //     match stream {
-    //         Ok(stream) => {
-    //             println!("Connection established! Responding to ping...");
-    //             handle_client(stream);
-    //         }
-    //
-    //         Err(e) => {
-    //             println!("An error occurred: {:?}", e);
-    //         }
-    //     }
-    // }
-
-
     match listener.accept().await? {
         Ok((_socket, addr)) => {
             println!("Connection established! {addr}...");
@@ -53,14 +39,3 @@ async fn handle_client(mut stream: TcpStream) -> JoinHandle<()> {
             }
     })
 }
-
-// fn handle_client(mut stream: TcpStream) {
-//     loop {
-//         let reads = stream.read(&mut [0; 256]).unwrap();
-//         if reads == 0 {
-//             break;
-//         }
-//
-//         stream.write(b"+PONG\r\n").unwrap();
-//     }
-// }
