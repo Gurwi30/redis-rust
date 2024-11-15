eb9cba1f3725dc31ae805a4b2afcb8e53c057c66
Gurwi <gs3335769@gmail.com>
2024-11-15 09:25:10 +0100

    test

--- src/parser.rs
+++ src/parser.rs
@@ -21,7 +21,7 @@ impl Value {
             Value::Boolean(b) => format!("#{}\r\n", b.to_string().chars().next().unwrap()),
             Value::Integer(i) => format!(":{}\r\n", i),
             Value::Array(arr) => format!("*{}\r\n{}\r\n", arr.len(), arr.iter().map(|v| v.clone().serialize()).collect::<Vec<_>>().join("\r\n")),
-            Value::NullBulkString => "-1\r\n".to_string(),
+            Value::NullBulkString => "$-1\r\n".to_string(),
             Value::Null => "_\r\n".to_string()
             //_ => panic!("Tried to serialize unserializable value!")
         }
