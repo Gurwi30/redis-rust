ffdf5c9336484b666bf3e2ab1a7f1aa9d99ea449
Gurwi <gs3335769@gmail.com>
2024-11-15 20:39:31 +0100

    test

--- src/storage.rs
+++ src/storage.rs
@@ -92,32 +92,10 @@ impl RDBFile {
         let contents = fs::read(file_path).unwrap();
         println!("contents: {:?}", contents);
 
-        let (redis_version_number, read_bytes) = read_from_until(&contents, 0, 0xFA).map(|data| (String::from_utf8(Vec::from(data.0)).unwrap(), data.1)).unwrap();
-        println!("RBD File Header Version: {:?}", redis_version_number);
+        let resizedb_field_pos = contents.iter().position(| &b | b == 0xFB).unwrap();
 
-
-        // Skip the 0xFA byte and read the metadata
-        let metadata = &contents[read_bytes + 1..];
-
-        // Now, we need to read the attribute name and value
-        // if let Some(attribute_name_end) = metadata.iter().position(|&x| x == 0x2D) {
-        //     let attribute_name = &metadata[0..attribute_name_end];
-        //     let attribute_value = &metadata[attribute_name_end + 1..];
-        //
-        //     // Convert bytes to string
-        //     let attribute_name_str = String::from_utf8_lossy(attribute_name);
-        //     let attribute_value_str = String::from_utf8_lossy(attribute_value);
-        //
-        //     println!("Attribute Name: {}", attribute_name_str);
-        //     println!("Attribute Value: {}", attribute_value_str);
-        // }
-
-        println!("Metadata: {:?}", metadata);
-        println!("Metadata 1 Length: {:?}", metadata[0] as usize);
-
-        //
-        // let metadata = read_from_until(&contents, read_bytes, 0xFE).map(|data| (data.0, data.1)).unwrap();
-        // println!("RBD Metadata: {}", String::from_utf8_lossy(metadata.0));
+        let len = contents[resizedb_field_pos + 1];
+        println!("resizedb_field_pos len: {:?}", len);
 
         Ok(
             RDBFile {
@@ -139,3 +117,30 @@ fn read_from_until(data: &[u8], start: usize, until: i32) -> Option<(&[u8], usiz
 
     None
 }
+
+// let (redis_version_number, read_bytes) = read_from_until(&contents, 0, 0xFA).map(|data| (String::from_utf8(Vec::from(data.0)).unwrap(), data.1)).unwrap();
+// println!("RBD File Header Version: {:?}", redis_version_number);
+
+
+// Skip the 0xFA byte and read the metadata
+// let metadata = &contents[read_bytes + 1..];
+
+// Now, we need to read the attribute name and value
+// if let Some(attribute_name_end) = metadata.iter().position(|&x| x == 0x2D) {
+//     let attribute_name = &metadata[0..attribute_name_end];
+//     let attribute_value = &metadata[attribute_name_end + 1..];
+//
+//     // Convert bytes to string
+//     let attribute_name_str = String::from_utf8_lossy(attribute_name);
+//     let attribute_value_str = String::from_utf8_lossy(attribute_value);
+//
+//     println!("Attribute Name: {}", attribute_name_str);
+//     println!("Attribute Value: {}", attribute_value_str);
+// }
+
+// println!("Metadata: {:?}", metadata);
+// println!("Metadata 1 Length: {:?}", metadata[0] as usize);
+//
+// //
+// // let metadata = read_from_until(&contents, read_bytes, 0xFE).map(|data| (data.0, data.1)).unwrap();
+// // println!("RBD Metadata: {}", String::from_utf8_lossy(metadata.0));
