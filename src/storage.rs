d13c12a26d59f3874d89fa97ddfe3555f0aeb086
Gurwi <gs3335769@gmail.com>
2024-11-16 15:25:46 +0100

    test

--- src/storage.rs
+++ src/storage.rs
@@ -2,7 +2,9 @@ use crate::parser::Value;
 use anyhow::{anyhow, Result};
 use std::collections::HashMap;
 use std::fs;
+use std::io::Read;
 use std::time::Instant;
+use bytes::Buf;
 
 pub struct Storage {
     storage: HashMap<String, DataContainer>
@@ -89,19 +91,49 @@ impl RDBFile {
             return Err(anyhow!("File does not end with '.rdb'"));
         }
 
-        let contents = fs::read(file_path).unwrap();
+        let contents = &fs::read(file_path)?;
         println!("contents: {:?}", contents);
 
         let resizedb_field_pos = contents.iter().position(| &b | b == 0xFB).unwrap();
         let mut pos = resizedb_field_pos + 1;
 
-        let len = contents[pos] as usize;
-        println!("len: {:?}", len);
-
-        let values_amount = &contents[pos..(pos + len)];
-        println!("value_i_think: {:?}", values_amount);
+        let hash_table_size = contents[pos] as usize;
+        println!("len: {:?}", hash_table_size);
+        pos += 1;
+        let expire_hash_table_size = contents[pos] as usize;
 
         pos += 1;
+
+        for _ in 1..(hash_table_size + expire_hash_table_size) {
+            let mut expiration_in_mills: Option<u128> = None;
+            pos += 1;
+
+            if contents[pos] == 0xFD {
+                let slice = &contents[pos..pos + 4];
+                expiration_in_mills = Some((read_length_encoded_int(slice)? as u128) * 1000);
+                pos += 4;
+            }
+
+            if contents[pos] == 0xFC {
+                let slice = &contents[pos..pos + 8];
+                expiration_in_mills = Some((read_length_encoded_int(slice)? as u128) * 1000);
+                pos += 8;
+            }
+
+            let value_type = contents[pos];
+            pos += 1;
+
+            let mut slice = &contents[pos..];
+            let (key, read_bytes) = read_length_encoded_string(slice)?;
+            pos += read_bytes;
+
+            slice = &contents[pos..];
+            let (value, read_bytes) = read_length_encoded_string(slice)?;
+            pos += read_bytes;
+
+            println!("key: {:?}", key);
+            println!("value: {:?}", value);
+        }
 
         Ok(
             RDBFile {
@@ -122,6 +154,33 @@ fn read_from_until(data: &[u8], start: usize, until: i32) -> Option<(&[u8], usiz
     }
 
     None
+}
+
+fn read_length_encoded_int(bytes: &[u8]) -> Result<u64> {
+    let first_byte = bytes[0];
+
+    match first_byte {
+        b if b < 0x80 => Ok(u64::from(b)),
+        b if b < 0xC0 => {
+            let second_byte = bytes[1];
+            Ok(((b as u64 & 0x3F) << 8) | second_byte as u64)
+        }
+
+        0xC0 => {
+            let mut buff: [u8; 4] = [0; 4];
+            bytes.reader().read_exact(&mut buff)?;
+            Ok(u32::from_be_bytes(buff) as u64)
+        }
+
+        0xD0 => Ok(u64::from_be_bytes(bytes.try_into()?)),
+
+        _ => Err(anyhow!("Error reading length encoded integer!"))
+    }
+}
+
+fn read_length_encoded_string(bytes: &[u8]) -> Result<(String, usize)> {
+    let str_len = bytes[0];
+    Ok((String::from_utf8(bytes[1..(str_len as usize)].to_vec()).map_err(|_| anyhow!("Error reading length encoded string!"))?, (str_len + 1) as usize))
 }
 
 // let (redis_version_number, read_bytes) = read_from_until(&contents, 0, 0xFA).map(|data| (String::from_utf8(Vec::from(data.0)).unwrap(), data.1)).unwrap();
