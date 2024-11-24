a6ac5a09c1726dcb4b92d0a64bb89fac1a23f9e5
Gurwi <gs3335769@gmail.com>
2024-11-24 16:38:09 +0100

    test

--- src/storage.rs
+++ src/storage.rs
@@ -146,7 +146,7 @@ impl RDBFile {
                 };
 
                 cursor += 2; // ADDED ONE TO SKIP VALUE TYPE
-                println!("expiration: {:?}", buffer[cursor..]);
+                println!("expiration: {}", String::from_utf8_lossy(&buffer[cursor..]));
 
                 let (key, key_length) = read_length_encoded_string(&buffer[cursor..])?;
                 cursor += key_length;
