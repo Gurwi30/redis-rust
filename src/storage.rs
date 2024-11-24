3d6068b21e83f8112bc7cb890f9fdde162872b1b
Gurwi <gs3335769@gmail.com>
2024-11-24 16:41:31 +0100

    test

--- src/storage.rs
+++ src/storage.rs
@@ -124,7 +124,7 @@ impl RDBFile {
 
             cursor += 1;
             let hash_table_size = buffer[cursor] as usize;
-            cursor += 1;
+            cursor += 2; // SKIPPED EXPIRE HASH TABLE SIZE
 
             println!("Hash table size: {}", hash_table_size);
 
@@ -146,7 +146,6 @@ impl RDBFile {
                 };
 
                 cursor += 2; // ADDED ONE TO SKIP VALUE TYPE
-                println!("expiration: {:?}", buffer[..cursor]);
 
                 let (key, key_length) = read_length_encoded_string(&buffer[cursor..])?;
                 cursor += key_length;
