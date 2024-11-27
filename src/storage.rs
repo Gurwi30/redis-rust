6f8fa280bbeb87c85557d4a26b97d7c4e401b13f
Gurwi <gs3335769@gmail.com>
2024-11-27 19:16:05 +0100

    test

--- src/storage.rs
+++ src/storage.rs
@@ -142,8 +142,8 @@ impl RDBFile {
                             0xFC => {
                                 let slice: [u8; 8] = buffer[cursor + 1..cursor + 9].try_into()?;
                                 cursor += 8;
-                                println!("bytes: {}", u64::from_be_bytes(slice));
-                                Some(Duration::from_millis(u64::from_be_bytes(slice)).as_millis())
+                                println!("bytes: {}", u64::from_le_bytes(slice));
+                                Some(Duration::from_millis(u64::from_le_bytes(slice)).as_millis())
                             }
 
                             _ => None
